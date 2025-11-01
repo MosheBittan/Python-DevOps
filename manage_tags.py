5a41c9c1f89c1e2838b81f668a524ea77c7c7a8b
MosheBittan <moshebitgit@gmail.com>
2025-11-02 00:29:50 +0200

    Update manage_tags.py

--- manage_tags.py
+++ manage_tags.py
@@ -12,13 +12,33 @@ def manage_tags(existing_tags: dict, *simple_tags, **key_value_tags) -> dict:
     Returns:
         A new dictionary with all tags merged.
     """
+    
+    # print(f"Required Tags: {existing_tags}")
+    # print(f"Simple Extras (Tuple): {simple_tags}")
+    # print(f"Key-Value Extras (Dictionary): {key_value_tags}")
+    
     # TODO: Implement the tagging logic.
     # 1. Do not modify the original dictionary
-    all_tags = existing_tags.copy()
+    new_tags = existing_tags.copy()
     # 2. Process the 'simple_tags' (*args).
-    for tag in simple_tags:
-        all_tags[tag] = 'true'
+    #it will work without set but if you have million on simple_tags this will remove only to unique and then do the proccess which more fast and performance
+    for value in set(simple_tags)
+        new_tags.update({value:"true"})
     # 3. Process the 'key_value_tags' (**kwargs).
-    all_tags.update(key_value_tags)
+    new_tags.update(key_value_tags)
     # 4. Return the new, merged dictionary.
-    return all_tags
+    return new_tags
+    
+
+
+initial = {'owner': 'dev-team', 'env': 'dev'}
+final_tags = manage_tags(
+    initial,
+    'billable',              # A simple tag
+    'critical',
+    'billable',                            # Another simple tag
+    env='staging',           # A key-value tag that overwrites an existing key
+    cost_center='xyz-123'    # A new key-value tag
+)
+print("#######\n")
+print(final_tags)
